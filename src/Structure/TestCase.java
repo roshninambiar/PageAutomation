a1a6000fa4f8e042a19020a7b15090e1311255b3
roshninambiar <nambiarroshni96@gmail.com>
2018-01-22 10:28:29 +0530

    Added success and failure condition for each case

--- src/Structure/TestCase.java
+++ src/Structure/TestCase.java
@@ -1,10 +1,23 @@
 package Structure;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+//THIS CLASS CONTAINS THE VARIABLES THAT NEEDS TO BE INITIALIZED FOR INITIATING THE LOADING AND TESTING OF PAGE
 class test{
 	String testAction;
 	String field;
@@ -13,6 +26,7 @@ class test{
 	int successCondition;
 	int failureCondition;	
 	
+	//PARAMETRIZED CONSTRUCTOR FOR INITIALIZING
 	public test(String testAction,String field,String fieldReference,String fieldValue,int successCondition,int failureCondition){
 	//void test(){
 		this.testAction = testAction;
@@ -26,59 +40,101 @@ class test{
 }
 
 public class TestCase {
-	public static void main(String args[]){
+	public static void main(String args[]) throws Exception{
 		
 		WebElement we = null;
-				
+			
+		//SET THE DRIVER LOCATION 
 		System.setProperty("webdriver.gecko.driver", "/home/devteam/Documents/AllDrivers/geckodriver");
+		//INITIALIZE THE DRIVER
 		WebDriver driver = new FirefoxDriver();
-		//WebElement we;
+	
+		//ARRAY OF OBJECTS OF THE TEST CLASS
 		test[] testcase = new test[110];
-		//test action, field, fieldReference, field Value, success condition, failure condition
-		testcase[0] = new test("loadPage", "https://www.facebook.com", "", "", 1, 8);
-		testcase[1] = new test("findElement", "email", "id", "", 2, 8);
-		testcase[2] = new test("fillValue", "", "", "newaccount@gmail.com", 3, 8);
-		testcase[3] = new test("findElement", "pass", "id", "", 4, 8);
-		testcase[4] = new test("fillValue", "", "", "newaccount", 5, 8);
-		testcase[5] = new test("findElement", "loginbutton", "id", "", 6, 8);
-		testcase[6] = new test("fillValue", "click", "", "", 8, 8);
-		//testcase[7] = new test("loadPage", "", "", "", 1, 8);
-		testcase[8] = new test("End","https://gmail.com", "", "", 1, 3);
+
+		//THE TEST CASES ARE INITIALIZED
+		testcase[0] = new test("loadPage", "https://www.facebook.com", "", "", 1, 100);
+		testcase[1] = new test("findElement", "email", "id", "", 2, 100);
+		testcase[2] = new test("fillValue", "", "", "newaccount@gmail.com", 3, 100);
+		testcase[3] = new test("findElement", "pass", "id", "", 4, 100);
+		testcase[4] = new test("fillValue", "", "", "newaccount", 5, 100);
+		testcase[5] = new test("findElement", "loginbutton", "id", "", 6, 100);
+		testcase[6] = new test("fillValue", "click", "", "", 7, 100);
+		testcase[7] = new test("loadPage","https://www.gmail.com", "", "", 8, 100);
 		
-		String a = "click";
+
 		
 		System.out.println("Test");
 		int i = 0;
 		
+		//ITERATE THE OBJECTS TILL TEST ACTION IS 'END' 
 		while(testcase[i].testAction!="End"){
 			switch(testcase[i].testAction){
 			
+				//LOADS THE PAGE BASED ON THE STATUS CODE
 				case "loadPage":
 					System.out.println("load page");
-					driver.get(testcase[i].field);
-					i = testcase[i].successCondition;
+					boolean status = checkStatusCode(testcase[i].field);	//CALL THE FUNCTION TO GET THE HTTP RESPONSE CODE
+					if(status == true){
+						driver.get(testcase[i].field);
+						i = testcase[i].successCondition;
+					}
+					else{
+						i = testcase[i].failureCondition;
+					}
 					break;
 				
+				//FINDS THE ELEMENTS, CLASSIFY BASED ON THE ATTRIBUTE
 				case "findElement":
 					switch(testcase[i].fieldReference){
 						case "id":
-							we = driver.findElement(By.id(testcase[i].field));
+							if((driver.findElement(By.id(testcase[i].field))).isDisplayed()){
+								we = driver.findElement(By.id(testcase[i].field));
+								i = testcase[i].successCondition;
+							}
+							else{
+								i = testcase[i].failureCondition;
+							}
 							break;
 						
 						case "name":
-							we = driver.findElement(By.name(testcase[i].field));
+							if((driver.findElement(By.name(testcase[i].field))).isDisplayed()){
+								we = driver.findElement(By.name(testcase[i].field));
+								i = testcase[i].successCondition;
+							}
+							else{
+								i = testcase[i].failureCondition;
+							}
 							break;
 							
 						case "link":
-							we = driver.findElement(By.linkText(testcase[i].field));
+							if((driver.findElement(By.linkText(testcase[i].field))).isDisplayed()){
+								we = driver.findElement(By.linkText(testcase[i].field));
+								i = testcase[i].successCondition;
+							}
+							else{
+								i = testcase[i].failureCondition;
+							}
 							break;
 							
 						case "class":
-							we = driver.findElement(By.className(testcase[i].field));
+							if((driver.findElement(By.className(testcase[i].field))).isDisplayed()){
+								we = driver.findElement(By.className(testcase[i].field));
+								i = testcase[i].successCondition;
+							}
+							else{
+								i = testcase[i].failureCondition;
+							}
 							break;
 							
 						case "partial link":
-							we = driver.findElement(By.partialLinkText(testcase[i].field));
+							if((driver.findElement(By.partialLinkText(testcase[i].field))).isDisplayed()){
+								we = driver.findElement(By.partialLinkText(testcase[i].field));
+								i = testcase[i].successCondition;
+							}
+							else{
+								i = testcase[i].failureCondition;
+							}
 							break;
 					}
 				
@@ -89,11 +145,9 @@ public class TestCase {
 				case "fillValue":
 					System.out.println("Fill Values" + i);
 					if((testcase[i].field).equals("click")){
-						System.out.println("Red");
 						we.click();	
 					}
 					else{
-						System.out.println("Yellow");
 						we.sendKeys(testcase[i].field);
 					}
 					we.sendKeys(testcase[i].fieldValue);
@@ -101,7 +155,6 @@ public class TestCase {
 					break;
 				
 				case "End":
-					System.out.println("End");
 					break;
 				
 				default:
@@ -111,4 +164,27 @@ public class TestCase {
 		}
 	}
 
+
+	public static boolean checkStatusCode(String baseurl) throws ClientProtocolException, IOException{
+		
+		boolean ret= true;
+		
+		//Get status code
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpGet httpget = new HttpGet(baseurl);
+	    HttpResponse response = httpclient.execute(httpget);
+		StatusLine sl = response.getStatusLine();
+		int code = sl.getStatusCode();
+							
+		//check if status code is a success or failure condition
+		if(code == 200){
+			ret = true;
+		}
+		else if(code == 404){
+			ret = false;
+		}
+		return ret;
+		
+	
+	}
 }
